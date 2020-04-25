27be00a16bbbdcaa4a69e37617c3e574b72b0d2a
andinaeta <mikorlk4328@gmail.com>
2020-04-25 16:12:02 +0700

    first commit

---
+++ precache-manifest.c419e93408e2655abb25b91cbfde57cc.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3bcc87e2a73419bc8ff942d728b9816",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ed555c42871a5c2bdc8",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "12ad3fb3ea8aee2bf9eb",
+    "url": "/static/css/main.98711a09.chunk.css"
+  },
+  {
+    "revision": "5ed555c42871a5c2bdc8",
+    "url": "/static/js/2.2c46052d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2c46052d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12ad3fb3ea8aee2bf9eb",
+    "url": "/static/js/main.13e298c4.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "f7e61417e7d1a968e9e01aa85f20a90e",
+    "url": "/static/media/SF-UI-Display-Regular.f7e61417.otf"
+  },
+  {
+    "revision": "1d2242832c43423d2c3f31d6fcd9f50e",
+    "url": "/static/media/account-light-icon.1d224283.svg"
+  },
+  {
+    "revision": "694c11552bd7b8d719bca34f79a13592",
+    "url": "/static/media/account-menu-icon.694c1155.svg"
+  },
+  {
+    "revision": "c2a6e3b3a19d8a452941cbf5e8275d17",
+    "url": "/static/media/congrats.c2a6e3b3.svg"
+  },
+  {
+    "revision": "cac3379e4666ab7be7858d2df0520e03",
+    "url": "/static/media/dummy-image.cac3379e.svg"
+  },
+  {
+    "revision": "f87b25547233ea37599ef24ccccb068f",
+    "url": "/static/media/gallery-icon.f87b2554.svg"
+  },
+  {
+    "revision": "5505b22f1eda3012f6b682e7693c8666",
+    "url": "/static/media/home-menu-icon.5505b22f.svg"
+  },
+  {
+    "revision": "3023328c00cbe02a72062ae3e9b61053",
+    "url": "/static/media/key-icon.3023328c.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "410247796e04ec10fb7911f9082d59b5",
+    "url": "/static/media/minus-light-icon.41024779.svg"
+  },
+  {
+    "revision": "714341aff3d8fae09df0f026f14ba1c9",
+    "url": "/static/media/move-to-icon.714341af.svg"
+  },
+  {
+    "revision": "adad508f62ab0532fb08071b0542559d",
+    "url": "/static/media/notification-image-undraw.adad508f.svg"
+  },
+  {
+    "revision": "7620ec08156627e22551233623168fd7",
+    "url": "/static/media/phone-icon.7620ec08.svg"
+  },
+  {
+    "revision": "b3ffa01f2dbe26da5cf15408cfa83e8c",
+    "url": "/static/media/plus-cyan-icon.b3ffa01f.svg"
+  },
+  {
+    "revision": "df4d40e0af53b727886d8bdcbbe16b8e",
+    "url": "/static/media/plus-light-icon.df4d40e0.svg"
+  },
+  {
+    "revision": "52c7fcfb5b4559a59983072e1fdb9cfb",
+    "url": "/static/media/product-menu-icon.52c7fcfb.svg"
+  },
+  {
+    "revision": "49470e91788ac9e5e89cdf24a8fcf9bf",
+    "url": "/static/media/right-arrow-icon.49470e91.svg"
+  },
+  {
+    "revision": "bc99a45fdcf4efc0b5fd92655e47f19c",
+    "url": "/static/media/search-icon.bc99a45f.svg"
+  },
+  {
+    "revision": "fe4a533857e01166b940e2d1c56057c9",
+    "url": "/static/media/search-menu-icon.fe4a5338.svg"
+  },
+  {
+    "revision": "511d91471244529ae43e7a9aaedf2255",
+    "url": "/static/media/troley-icon.511d9147.svg"
+  }
+]);
