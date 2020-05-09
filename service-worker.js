79d497d85d97d58e3e5250088e360b1e8cc06b07
andinaeta <mikorlk4328@gmail.com>
2020-05-09 07:19:31 +0700

    updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4dcb028f230c423bec7015b79bd60175.js"
+  "/precache-manifest.c3faaf58f3e5b7562450ec262c0265d2.js"
 );
 
 self.addEventListener('message', (event) => {
